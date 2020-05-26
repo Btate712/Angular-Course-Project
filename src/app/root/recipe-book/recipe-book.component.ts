d57c0b5c2509e239110583fb35fd48b6cf7dc85b
Bob Tate <btate712@gmail.com>
2020-05-25 23:36:02 -0500

    started replacing data passing using event and property binding to use of services

--- src/app/root/recipe-book/recipe-book.component.ts
+++ src/app/root/recipe-book/recipe-book.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
+
 @Component({
   selector: 'app-recipe-book',
   templateUrl: './recipe-book.component.html',
   styleUrls: ['./recipe-book.component.css']
 })
 export class RecipeBookComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
-  }
-
-  setRecipe(recipe: Recipe) {
-    this.recipe = recipe;
+    this.recipeService.recipeSelected.subscribe(
+      (recipe: Recipe) => {
+        this.recipe = recipe;
+      }
+    )
   }
 }
